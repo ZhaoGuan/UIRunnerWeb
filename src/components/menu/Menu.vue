1e1d444adac5c4818067ae5d5886082e826e68f7
ZhaoGuan <guanzhaogz@gmail.com>
2022-04-01 18:40:11 +0800

    修正menu layout

--- src/components/menu/Menu.vue
+++ src/components/menu/Menu.vue
@@ -4,35 +4,35 @@
            background-color="#545c64"
            text-color="#fff"
            router :collapse="isCollapse" style="text-align: left;height: calc(98vh)">
-      <div v-for="(item,index) in theRoutes" :key="index">
-          <el-submenu :index="index+''" v-if="item.children && item.children.length > 0">
-            <template slot="title">
-              <i :class="item.meta.icon"/>
-              <span class="menuTitle">{{ item.meta.title }}</span>
-            </template>
-            <el-menu-item-group v-for="(childItem,childIndex) in item.children" :key="childIndex">
-              <el-menu-item v-if="!childItem.meta.hidden"
-                            :index="index+''+childIndex"
-                            @click="resolvePath(item.path+'/'+childItem.path)">
-                <i :class="childItem.meta.icon"/>
-                <span>{{ childItem.meta.title }}</span>
-              </el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>
-          <el-menu-item v-show="!item.meta.hidden" style="text-align: left"
-                        :index="index+''" v-else @click="resolvePath(item.path)">
-            <i :class="item.meta.icon"/>
-            <template slot="title">
-              <span class="menuTitle">{{ item.meta.title }}</span>
-            </template>
+    <div v-for="(item,index) in theRoutes" :key="index">
+      <el-submenu :index="index+''" v-if="item.children && item.children.length > 0">
+        <template slot="title">
+          <i :class="item.meta.icon"/>
+          <span class="menuTitle">{{ item.meta.title }}</span>
+        </template>
+        <el-menu-item-group v-for="(childItem,childIndex) in item.children" :key="childIndex">
+          <el-menu-item v-if="!childItem.meta.hidden"
+                        :index="index+''+childIndex"
+                        @click="resolvePath(item.path+'/'+childItem.path)">
+            <i :class="childItem.meta.icon"/>
+            <span>{{ childItem.meta.title }}</span>
           </el-menu-item>
-      </div>
-      <el-button v-if="hide" type="primary"
-                 style="width: 100%;border: none;border-radius: 0;position: absolute;bottom: 0;left: 0"
-                 @click="isCollapse=!isCollapse">
-        <i v-if="isCollapse" class="el-icon-d-arrow-right"/>
-        <i v-else class="el-icon-d-arrow-left"/>
-      </el-button>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-menu-item v-show="!item.meta.hidden" style="text-align: left"
+                    :index="index+''" v-else @click="resolvePath(item.path)">
+        <i :class="item.meta.icon"/>
+        <template slot="title">
+          <span class="menuTitle">{{ item.meta.title }}</span>
+        </template>
+      </el-menu-item>
+    </div>
+    <el-button v-if="hide" type="primary"
+               style="width: 100%;border: none;border-radius: 0;position: absolute;bottom: 0;left: 0"
+               @click="isCollapse=!isCollapse">
+      <i v-if="isCollapse" class="el-icon-d-arrow-right"/>
+      <i v-else class="el-icon-d-arrow-left"/>
+    </el-button>
   </el-menu>
 
 </template>
@@ -83,5 +83,12 @@ export default {
 
 </style>
 <style lang="scss" scoped>
+
+.el-submenu .el-menu-item {
+  height: 50px;
+  line-height: 50px;
+  padding: 0 45px;
+  min-width: 100px;
+}
 
 </style>
